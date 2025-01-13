472ac810c42296f3b7311dc9c6c7134f5fe24835
Viet Thanh <thanhnv@gmail.com>
2025-01-13 22:57:02 +0700

    ontap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductContainerComponent } from "./product-container/product-container.component";
 import { ProductDetailComponent } from "./product-container/product-detail/product-detail.component";
+import { FooterComponent } from './product-container/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductContainerComponent, ProductDetailComponent,CommonModule],
+  imports: [RouterOutlet, ProductContainerComponent,
+     ProductDetailComponent,CommonModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
